bf05f107e6eff287097db52e339ec37bd27f4d3a
Yadier <yadierg7@gmail.com>
2023-03-08 10:48:13 -0400

    Added Components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useRef } from "react";
+import Header from "./components/Header";
+import { Project } from "./components/Projects";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { Main } from "./components/Main";
+
 function App() {
+  const ref1 = useRef(null);
+
   return (
     <div>
-      <h1 className="bg-red-500">as</h1>
+      <Header></Header>
+      <Main></Main>
+      <Project></Project>
+      <About></About>
+      <Contact></Contact>
     </div>
   );
 }
