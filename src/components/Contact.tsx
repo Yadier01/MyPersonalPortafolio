02cc205147a63c6baca2c78055b2b7aebf9bc221
Yadier <yadierg7@gmail.com>
2023-03-09 17:28:07 -0400

    changed form verification to netlify forms

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,42 +1,26 @@
 import React from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-
-type Inputs = {
-  name: string;
-  email: string;
-  message: string;
-};
 
 export const Contact = React.forwardRef<HTMLDivElement, { text: string }>(
   ({ text }, ref) => {
-    const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm<Inputs>();
-
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
     return (
       <section className="h-screen  bg-zinc-900 text-white py-20" ref={ref}>
         <h2 className="text-5xl text-center">{text}</h2>
         <form
-          action=""
-          className="grid place-content-center h-full  text-black "
-          onSubmit={handleSubmit(onSubmit)}
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          className="grid place-content-center h-full  text-black m-auto"
         >
           <div className="bg-zinc-100 grid gap-3 p-7 rounded-lg ">
             <input
               type="text"
               placeholder="Name*"
-              {...register("name", { required: true })}
               className="bg-zinc-200 p-4 rounded-lg"
+              required
             />
-            {errors.name && <span>This field is required</span>}
             <input
               type="email"
               placeholder="Email*"
-              {...register("email", { required: true })}
               required
               className="bg-zinc-200 p-4 rounded-lg"
             />
@@ -45,7 +29,6 @@ export const Contact = React.forwardRef<HTMLDivElement, { text: string }>(
               required
               cols={30}
               rows={10}
-              {...register("message", { required: true })}
               minLength={10}
               className="bg-zinc-200 p-4 rounded-lg"
             ></textarea>
